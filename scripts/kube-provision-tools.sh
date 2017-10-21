b422dc3cde2de48a5ed4b8de48a4fb502d5ada3b
nmaupu <nmaupu@gmail.com>
2017-10-21 19:23:48 +0200

    - Added photoweb app + refacto

--- scripts/kube-provision-tools.sh
+++ scripts/kube-provision-tools.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-kubectl apply -f /workspace/kubernetes/admin
-
-# kube-system tools
-kubectl apply -Rf /workspace/kubernetes/kube-system/
+kubectl apply -Rf /workspace/kubernetes/home
 
