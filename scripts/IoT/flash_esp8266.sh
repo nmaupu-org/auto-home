06c668d76883b45b312e21463d0a969b2fa2912d
Nicolas Maupu <nmaupu@gmail.com>
2020-10-17 14:30:20 +0200

    Switching to tasmota

--- scripts/IoT/flash_esp8266.sh
+++ scripts/IoT/flash_esp8266.sh
@@ -4,10 +4,11 @@ set -e
 set -o pipefail
 
 : "${TMP_DIR:=/tmp}"
-: "${TASMOTA_VERSION:=v6.7.1}"
+: "${TASMOTA_VERSION:=v8.5.1}"
 : "${TTY:=/dev/tty}"
 : "${BAUD_RATE:=115200}"
 
 # Download and install
-curl -SsL -o "${TMP_DIR}/sonoff.bin" https://github.com/arendst/Tasmota/releases/download/${TASMOTA_VERSION}/sonoff.bin
-esptool.py -p ${TTY} write_flash -fm dio 0x00000 "${TMP_DIR}/sonoff.bin"
+curl -SsL -o "${TMP_DIR}/tasmota.bin" https://github.com/arendst/Tasmota/releases/download/${TASMOTA_VERSION}/tasmota.bin
+esptool.py -p ${TTY} erase_flash
+esptool.py -p ${TTY} write_flash -fm dio 0x00000 "${TMP_DIR}/tasmota.bin"
