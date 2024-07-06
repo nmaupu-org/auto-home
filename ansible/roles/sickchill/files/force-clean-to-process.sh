eaaabfd80bf1f79956ec1dccc2724c188298d6df
Nicolas Maupu <nmaupu@gmail.com>
2024-07-06 20:44:42 +0200

    Cleaning

--- ansible/roles/sickchill/files/force-clean-to-process.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/bash
-
-# Force cleaning toprocess directory if somehow files are left here and never deleted
-# Thus, avoiding directory to grow for nothing and sickchill to reprocess old files (when force post processing and ticking reprocess)
-
-: ${DIR:=/mnt/toprocess}
-: ${DAYS:=15}
-
-find "${DIR}" -type d -depth 1 -mtime +"${DAYS}" | xargs rm -rf
