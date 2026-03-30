21d79f0413c719303bf37253ca046895d218ff41
Nicolas Maupu <nmaupu@gmail.com>
2026-03-31 01:34:43 +0200

    Remove tmp nfs share

--- nix/modules/nas/nfs.nix
+++ nix/modules/nas/nfs.nix
@@ -7,7 +7,6 @@
     enable = true;
     exports = ''
       /tank/le-certs 192.168.12.0/24(sec=sys,no_root_squash,insecure,no_subtree_check)
-      /tank/backup-iot 192.168.12.0/24(sec=sys,no_root_squash,insecure,no_subtree_check,rw)
     '';
   };
 
