fd0b14757fb7d1c474bd66842f54d99b10006775
Nicolas Maupu <nmaupu@gmail.com>
2026-04-02 01:23:25 +0200

    Opening 1900 udp port (amazon alexa ssdp discovery)

--- nix/modules/iot/firewall-extras.nix
+++ nix/modules/iot/firewall-extras.nix
@@ -2,6 +2,7 @@
 
 {
   # Emulated Hue + Roku (home-assistant)
+  # 80: Hue/Roku API, 8060: Roku, 1900: SSDP/UPnP discovery (Alexa)
   networking.firewall.allowedTCPPorts = [ 8060 80 ];
-  networking.firewall.allowedUDPPorts = [ 8060 80 ];
+  networking.firewall.allowedUDPPorts = [ 8060 80 1900 ];
 }
