7bf578f711cf3761595589f35acaa84196cafbd2
Nicolas Maupu <nmaupu@gmail.com>
2026-04-10 13:04:48 +0200

    open port on iot

--- nix/modules/iot/firewall-extras.nix
+++ nix/modules/iot/firewall-extras.nix
@@ -3,6 +3,6 @@
 {
   # Emulated Hue + Roku (home-assistant)
   # 80: Hue/Roku API, 8060: Roku, 5540: Matter (matterbridge), 8283: matterbridge web UI
-  networking.firewall.allowedTCPPorts = [ 8060 80 8283 ];
+  networking.firewall.allowedTCPPorts = [ 8060 80 8283 8123 ];
   networking.firewall.allowedUDPPorts = [ 8060 80 5540 ];
 }
